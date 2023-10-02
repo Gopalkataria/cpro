92fc3008762481d3dfc47a131d2e0336be8e7559
Gopal Kataria <gopalkataria@outlook.com>
2023-10-02 18:32:14 +0530

    ass 3 : irritations prob fixed

--- Ass3/irritations.c
+++ Ass3/irritations.c
@@ -4,6 +4,7 @@
 #define input(x) scanf("%d", &x)
 #define print(x) printf("%d\n", x)
 
+
 #define min(a, b) ((a) < (b) ? (a) : (b)) // min function
 #define max(a, b) ((a) > (b) ? (a) : (b)) // max function
 
@@ -25,7 +26,7 @@ int irritation(int i)
     {
         return 0;
     }
-    if (MEM[i] = -1)
+    if (MEM[i] == 0)
     {
         MEM[i] = irrlist[i] + irritation(i + irrlist[i]);
     }
@@ -35,9 +36,8 @@ int irritation(int i)
 void testsolve()
 {
     input(n);
-    MEM = (int *)malloc(sizeof(int) * (n + 1));
-    for (int i = 1; i < n + 1; i++)
-        MEM[i] = -1;
+    MEM = (int *)calloc(sizeof(int) * (n + 1), sizeof(int));
+  
     irrlist = (int *)malloc(sizeof(int) * (n + 1));
     int irr, maxirr = 0;
     for (int i = 1; i < n + 1; i++)
--- Ass3/irritations.test.py
+++ Ass3/irritations.test.py
@@ -1,2 +1,12 @@
-from random import randint 
-t = randint(1,10**4) 
+from random import randint as randomint
+a = 10**4
+b = 10**4
+c = 10**5
+t = randomint(1,a) 
+print(t) 
+for x in range(t) :
+    n = randomint(1,b) 
+    print(n)
+    for y in range(n) :
+        print(randomint(1,c), end=" ")
+    print("\n")
